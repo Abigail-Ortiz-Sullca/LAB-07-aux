6772f8950f30a3d26e12d80fe434d95334ab1ed1
OrtizAbigail <amoscma469@gmail.com>
2024-04-13 11:31:00 -0400

    feature(common): first files added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { List } from './components/List';
+import { WrapperList } from './components/WrapperList';
+
+function App() {
+  return (
+    <>
+      <WrapperList>
+        <List/>
+      </WrapperList>
+    </>
+  );
+}
+
+export default App;
